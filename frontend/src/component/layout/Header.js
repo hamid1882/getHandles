c277f2c749d411dd6e7294a73e105f6f05bef8b5
hamid1882 <hussainhafeez1100@gmail.com>
2022-02-04 20:04:16 +0530

    started frontend

---
+++ frontend/src/component/layout/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { ReactNavbar } from "overlay-navbar";
+
+function Header() {
+  return <ReactNavbar burgerColor="black" navColor1="green" />;
+}
+
+export default Header;
