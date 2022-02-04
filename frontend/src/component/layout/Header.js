18e426b64c2c27a4eee8c7901ad44ef72e7c68df
hamid1882 <hussainhafeez1100@gmail.com>
2022-02-04 21:46:31 +0530

    added overlay-navbar

--- frontend/src/component/layout/Header.js
+++ frontend/src/component/layout/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { ReactNavbar } from "overlay-navbar";
 
 function Header() {
-  return <ReactNavbar burgerColor="black" navColor1="green" />;
+  return <ReactNavbar  />;
 }
 
 export default Header;
