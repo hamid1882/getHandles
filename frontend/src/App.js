65fbf0bb5994f7b31fe47895ec73f992873ef4df
hamid1882 <hussainhafeez1100@gmail.com>
2022-02-05 12:27:04 +0530

    added footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
-import Header from "./component/layout/Header.js";
+import React from "react";
+import Header from "./component/layout/Header/Header.js";
+import Footer from "./component/layout/Footer/Footer.js";
+
 import { BrowserRouter as Router } from "react-router-dom";
+import WebFont from "webfontloader";
+
 function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  },[]);
+
+
   return (
     <Router>
       <Header />
+      <Footer />
     </Router>
   );
 }
---
+++ frontend/src/component/layout/Footer/Footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+import playStore from "../../../images/playstore.png";
+import appStore from "../../../images/appstore.png";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer id="footer">
+      <div class="leftFooter">
+        <h4>DOWNLOAD OUR APP</h4>
+        <p>Download App for Android and IOS mobile phones.</p>
+        <img
+          src={playStore}
+          alt="playstore"
+          style={{ marginBottom: "-2em" }}
+        />
+        <img src={appStore} alt="playstore" />
+      </div>
+
+      <div class="midFooter">
+        <h1>GETHANDLES</h1>
+        <p>Using Skills on a right and efficient way is our first priority</p>
+
+        <p>Copyrights 2022 &copy; Hamidoo</p>
+      </div>
+
+      <div class="rightFooter">
+        <h4>Follow Us</h4>
+        <a href="https://twitter.com/hamid1882"> Twitter</a>
+        <a href="https://github.com/hamid1882"> github</a>
+        <a href="https://twitter.com/hamid1882"> LinkedIn</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
