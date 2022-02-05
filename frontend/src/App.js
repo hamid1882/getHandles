835b14e7ab5a29b397a1055c27cb3bad0b5be25f
hamid1882 <hussainhafeez1100@gmail.com>
2022-02-05 15:01:13 +0530

    added banner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import Header from "./component/layout/Header/Header.js";
 import Footer from "./component/layout/Footer/Footer.js";
 
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import WebFont from "webfontloader";
+import Home from "./component/Home/Home.js";
 
 function App() {
   React.useEffect(() => {
@@ -12,13 +13,26 @@ function App() {
         families: ["Roboto", "Droid Sans", "Chilanka"],
       },
     });
-  },[]);
-
+  }, []);
 
   return (
     <Router>
-      <Header />
-      <Footer />
+      <>
+        <div>
+          <Header />
+          <Home />
+          <Footer />
+        </div>
+        <Routes>
+          <Route path="/" element={Header}></Route>
+        </Routes>
+        <Routes>
+          <Route path="/" element={Home}></Route>
+        </Routes>
+        <Routes>
+          <Route path="/" element={Footer}></Route>
+        </Routes>
+      </>
     </Router>
   );
 }
